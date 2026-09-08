8ab3f0b7089309c86c33546a8256d4835fcce868
pvarunraj05-arch <p.varunraj05@gmail.com>
2026-09-08 10:35:33 +0530

    Update gpio.h

--- task2/submission/gpio.h
+++ task2/submission/gpio.h
@@ -2,7 +2,7 @@
 #define GPIO_H
 #include <ch32v00x.h>
 
-#define TEST_PIN 4 // Matches physical label PD6 on the board 
+#define TEST_PIN 4 // Matches physical label PD4 on the board 
 
 void gpio_init(void);
 void gpio_toggle(void);
